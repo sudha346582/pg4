2305bd866604fbdef53c639291f9387bec35fbdb
sudha <01fe24bca210@kletech.ac.in>
2025-10-30 09:59:00 -0700

    Math operation

---
+++ mat.py
@@ -0,0 +1,18 @@
+# Program to perform addition and subtraction of two numbers
+
+# Input from user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1/num2
+
+# Display results
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
